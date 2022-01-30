c04e8840269eb47893e0036c5394a51889c1a175
GMaffi <98100197+GMaffi@users.noreply.github.com>
2022-01-30 19:29:07 +0000

    Checked tests work

--- test_geo_1B+C.py
+++ test_geo_1B+C.py
@@ -2,20 +2,28 @@ from floodsystem.geo import stations_by_distance
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
+from haversine import haversine as hs
+
 
 
 def test_stations_by_distance():
+    '''tests that each station in the list resturned by the stations_by_distance function
+    has a distance higher than the one before it'''
 
     coord = (0, 0)
     stations = stations_by_distance(build_station_list(), coord)
 
     for i in range(1, len(stations)):
-        assert stations[i, 1] > stations[i-1, 1]
+        current_station = stations[i]
+        previous_station = stations[i-1]
+        assert current_station[1] >= previous_station[1]
 
 def test_stations_within_radius():
-    coord = (50, 0)
+    '''tests that the distance from each station returned by the stations_within_radius
+    function is less than the specified distance from the coordinate provided'''
+    coord = (52.2, 0)
     radius = 20
-    stations = stations_within_radius(build_station_list(), coord, )
+    stations = stations_within_radius(build_station_list(), coord, radius)
     for i in stations:
-        assert i[1] <= radius
-        
+        assert  hs(i.coord, coord) <= radius
+
