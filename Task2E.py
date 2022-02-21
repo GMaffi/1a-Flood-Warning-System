6ad10b0aa38e87c91e1748420637d611d2d8297f
GMaffi <98100197+GMaffi@users.noreply.github.com>
2022-02-21 17:51:33 +0000

    2E started

---
+++ Task2E.py
@@ -0,0 +1,22 @@
+import datetime
+from floodsystem.datafetcher import fetch_measure_levels
+
+from floodsystem.station import MonitoringStation
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.utils import sorted_by_key
+from floodsystem.plot import plot_water_levels
+
+stations = build_station_list()
+update_water_levels(stations)
+
+N = 5
+stations_by_rel_level = stations_highest_rel_level(stations, N)
+
+for i in stations_by_rel_level:
+    station = i[0]
+
+    dt = 10
+    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+
+    plot_water_levels(station, dates, levels)
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -3,5 +3,4 @@ from datetime import datetime, timedelta
 
 def plot_water_levels(station, dates, levels):
     plt.plot(dates, levels)
-
     plt.show()
