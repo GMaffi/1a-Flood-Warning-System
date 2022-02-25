63ec80a9086afca4ac8a6fa56a9a24b17287c218
Edward Townsend <ejpt2@cam.ac.uk>
2022-02-25 11:15:59 +0000

    task 2g complete

--- Task2G.py
+++ Task2G.py
@@ -14,11 +14,12 @@ Use extrapolation to predict the maximum water level expected and then assign ra
 
 """
 from floodsystem.station import MonitoringStation
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.analysis import polyfit
+from floodsystem.analysis import polyfit_coeff
 import datetime
+import numpy as np
 
 stations = build_station_list()
 update_water_levels(stations)
@@ -29,21 +30,41 @@ high = 4.0
 moderate = 2.5
 low = 1.5
 
-# generate list of stations above a threshold of 0.8m
-tol = 0.8
-risk_stations = []
-stations_over_threshold = stations_level_over_threshold(stations, tol)
-for i in stations_over_threshold:
-    risk_stations.append(i[0].name)
+# initial time period we want to examine (whole time period = 2*dt days)
+dt = 2
 
-print(risk_stations)
+# generate list of stations above a threshold of 0.8m
+# fetch_measure_levels funciton only works when tol above a certain value - can only take a maximum number of stations
+# therefore use stations_by_rel_level
+N = 50
+stations_by_rel_level = stations_highest_rel_level(stations, N, 20)
 
 # generate list of tuples of polynomial coefficients, using value of dt and p from 2f intially
 p_coeff = []
-for i in stations_over_threshold:
+for i in stations_by_rel_level:
     station = i[0]
-    dt = 2
     dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-    p_coeff.append(polyfit(dates, levels, 4))
+    p_coeff.append(polyfit_coeff(dates, levels, 4))
 
-print(p_coeff)
+# compute the maximum water level expected when the data is extrapolated dt days into future
+x = np.linspace(0, dt, 10)
+max_water_level = []
+for i in p_coeff:
+    poly = np.poly1d(i)
+    y = poly(x)
+    max_water_level.append(max(y))
+
+# print the flood warning risk and river
+for x, y in zip(stations_by_rel_level, max_water_level):
+    station = x[0]
+    if y >= severe:
+        print("SEVERE FLOOD WARNING RISK ON " + station.river)
+    elif y >= high:
+        print("HIGH FLOOD WARNING RISK ON " + station.river)
+    elif y >= moderate:
+        print("MODERATE FLOOD WARNING ALERT ON " + station.river)
+    elif y >= low:
+        print("LOW FLOOD WARNING ALERT ON " + station.river)
+
+# Could use 2f to test the values of dt and p to give the optimum polynomial fit over a time range 2*dt
+# Could see if we could increase N, the number of stations being examined, or try to create a list of stations over a minimum value
--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -7,3 +7,8 @@ def polyfit(dates, levels, p):
     poly = np.poly1d(p_coeff)
     d0 = d_float[0]
     return (poly, d0)
+
+def polyfit_coeff(dates,levels,p):
+    d_float = matplotlib.dates.date2num(dates)
+    p_coeff = np.polyfit(d_float - d_float[0], levels, p)
+    return p_coeff
